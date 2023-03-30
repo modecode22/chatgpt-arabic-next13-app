6b3172178c397ccf77ed0a3d6d5658dcdee92fd1
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-03-30 02:36:36 +0100

    finished - now => open source it

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,23 +6,26 @@ import {getFirestore} from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
- apiKey: process.env.API_KEY,
- authDomain: process.env.AUTH_DOMAIN,
- projectId: process.env.PROJECT_ID,
- storageBucket: process.env.STORAGE_BUCKET,
- messagingSenderId: process.env.MESSAGING_SENDER_ID,
- appId: process.env.APP_ID,
-  // apiKey: "AIzaSyAjZBLacayBgkB_uDnhm4SnCIAb5vptJCg",
-  // authDomain: "gptapp-9d363.firebaseapp.com",
-  // projectId: "gptapp-9d363",
-  // storageBucket: "gptapp-9d363.appspot.com",
-  // messagingSenderId: "840193690881",
-  // appId: "1:840193690881:web:ac10955462286946491f00",
+  
+   apiKey: "AIzaSyAjZBLacayBgkB_uDnhm4SnCIAb5vptJCg",
+   authDomain: "gptapp-9d363.firebaseapp.com",
+   projectId: "gptapp-9d363",
+   storageBucket: "gptapp-9d363.appspot.com",
+   messagingSenderId: "840193690881",
+   appId: "1:840193690881:web:ac10955462286946491f00",
+    // apiKey: process.env.API_KEY,
+    // authDomain: process.env.AUTH_DOMAIN,
+    // projectId: process.env.PROJECT_ID,
+    // storageBucket: process.env.STORAGE_BUCKET,
+    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    // appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 const db = getFirestore(app)
 
 export {db}
