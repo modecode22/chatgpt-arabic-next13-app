2fe12c34aa40e2b26f7f6c7f5f39daebf54f3e6f
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-03-30 01:49:04 +0100

    env variables for open sourcing the application

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -4,12 +4,12 @@ import LogInBtn from "./LogInBtn";
 const LogInPage = () => {
   return (
     <main className=" flex flex-col justify-center items-center w-screen h-screen ">
-      <section className="backdrop-blur-md flex flex-col justify-around items-center transition-all w-80 aspect-[8/10] shadow-md hover:shadow-lg duration-100 p-5 bg-black/60  rounded-2xl border border-lime-50/20 hover:border-lime-500/50">
+      <section className="overflow-hidden backdrop-blur-md flex flex-col justify-around items-center transition-all w-80 aspect-[8/10] shadow-md hover:shadow-lg duration-100 p-5 bg-black/60  rounded-2xl border border-lime-50/20 hover:border-lime-500/50">
         <header className="font-bold text-2xl text-center flex flex-col justify-center items-center gap-5">
-          <h1>الموقع</h1>
-          <h1>الذي لن يساعدك في الدراسة</h1>
+      <h1>مرحبا بك في تطبيق </h1>
+          <h1 className="text-6xl">Fake gpt</h1>
         </header>
-
+        <div className="screen-overlay"></div>
         <LogInBtn />
       </section>
       <LogInAnime />
